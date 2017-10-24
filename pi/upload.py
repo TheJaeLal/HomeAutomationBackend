852e758d20db3e5127255e0113ba2300657e69a0
Jay Lal <jay.lal@somaiya.edu>
2017-10-24 14:32:51 +0530

    Added multithreading support to upload.put()

--- pi/upload.py
+++ pi/upload.py
@@ -3,7 +3,7 @@ import os
 
 #local_file_path is going to be of the format.. 'static/videos/filename.mp4'
 
-def put(local_file_path):
+def put(local_file_path, thread_name):
 
 	try:
 		remote_media_dir = os.path.dirname(local_file_path)
@@ -12,16 +12,16 @@ def put(local_file_path):
 
 		server = pysftp.Connection(host = "139.59.65.16",username = 'jay', port = 2222, private_key = path_to_key)
 
-		print("****Connection Successful*****")
+		print(thread_name,"****Connection Successful*****")
 
-		print("remote_media_dir = ",remote_media_dir)
+		print(thread_name,"remote_media_dir = ",remote_media_dir)
 		server.cwd(remote_media_dir)
 
-		print("Successful changed directory")
+		print(thread_name,"Successful changed directory")
 		server.put(local_file_path)
 
-		print("Successfully uploaded file.")
+		print(thread_name,"Successfully uploaded file.")
 
 	except FileNotFoundError:
-		print("FileNotFoundError")
+		print(thread_name,"FileNotFoundError")
 		server.close()
