9b553be12ae9991c046c7644f949bf7d7a249994
Jay Lal <jay.lal@somaiya.edu>
2017-10-25 11:07:52 +0530

    ADDed status and control to lights.py

--- pi/lights.py
+++ pi/lights.py
@@ -1,11 +1,25 @@
-import RPi.GPIO as GPIO ## Import GPIO library
-import sys
+ ## Import GPIO library
+import RPi.GPIO as GPIO
 
 led_pins = [7, 11]
 
-control = sys.argv[1].strip().upper()=='ON' 
-no = int(sys.argv[2])
+#light_no is an interger 0 or 1
+#Control is a boolean, True or False
+def control(light_no,control):
+	global led_pins
+	GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+	GPIO.setup(led_pins[light_no], GPIO.OUT) ## Setup GPIO Pin 7 to OUT
+	GPIO.output(led_pins[light_no],control)
 
-GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-GPIO.setup(led_pins[no], GPIO.OUT) ## Setup GPIO Pin 7 to OUT
-GPIO.output(led_pins[no],control)
+#Returns a dictionary of Response
+def get_status():
+	global led_pins
+
+	response = {"1":"OFF","2":"OFF"}
+	
+	if GPIO.input(led_pins[0]):
+		response["1"] = "ON"
+	if GPIO.input(led_pins[1]):
+		response["2"] = "ON"
+
+	return response
